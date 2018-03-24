d947b2bfcd577a424101324d2804b64644d83a6e
DmitryShaidurov <darbyofficial@gmail.com>
2018-03-24 12:57:28 +0500

    TaskDone

---
+++ LABA2/LABA2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        public static int BinarySearch(int[] array, int value)
+        {
+            if (array.Length == 0) return -2;
+            var left = 0;
+            var right = array.Length - 1;
+            while (left < right)
+            {
+                var middle = (right + left) / 2;
+                if (value <= array[middle])
+                    right = middle;
+                else left = middle + 1;
+            }
+            if (array[right] == value)
+                return right;
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            TestOneElement();
+            TestNegativeNumbers();
+            TestNonExistentElement();
+            TestRepeatedElements();
+            TestEmptyMassive();
+            TestBigMassive();
+            Console.ReadKey();
+        }
+        private static void TestOneElement()
+        {
+            //Тестирование поиска одного элемента в массиве из 5 элементов
+            int[] fiveElements = new[] { 3, 7, 14, 26, 44 };
+            if (BinarySearch(fiveElements, 7) != 1)
+                Console.WriteLine("! Поиск не нашёл число 7 среди чисел { 3, 7, 14, 26, 44 }");
+            else
+                Console.WriteLine("Поиск одного элемента в массиве из 5 элементов выполнен корректно");
+        }
+        private static void TestNegativeNumbers()
+        {
+            //Тестирование поиска в отрицательных числах 
+            int[] negativeNumbers = new[] { -5, -4, -3, -2 };
+            if (BinarySearch(negativeNumbers, -3) != 2)
+                Console.WriteLine("! Поиск не нашёл число -3 среди чисел {-5, -4, -3, -2}");
+            else
+                Console.WriteLine("Поиск среди отрицательных чисел работает корректно");
+        }
+        private static void TestNonExistentElement()
+        {
+            //Тестирование поиска отсутствующего элемента 
+            int[] numbers = new[] { -5, -4, -3, -2 };
+            if (BinarySearch(numbers, -1) >= 0)
+                Console.WriteLine("! Поиск нашёл число -1 среди чисел {-5, -4, -3, -2}");
+            else
+                Console.WriteLine("Поиск отсутствующего элемента вернул корректный результат");
+        }
+        private static void TestRepeatedElements()
+        {
+            //Тестирование поиска элемента, повторяющегося несколько раз 
+            int[] numbers = new[] { 3, 7, 7, 14, 44 };
+            if (BinarySearch(numbers, 7) != 1)
+                Console.WriteLine("! Поиск не нашёл число 7 среди чисел { 3, 7, 7, 14, 44 }");
+            else
+                Console.WriteLine("Поиск элемента, повторяющегося несколько раз работает корректно");
+        }
+        private static void TestEmptyMassive()
+        {
+            //Тестирование поиска в пустом массиве 
+            int[] numbers = new int[0];
+            if (BinarySearch(numbers, 7) != -2)
+                Console.WriteLine("! Поиск в пустом массиве работает не корректно");
+            else
+                Console.WriteLine("Поиск в пустом массиве работает корректно");
+        }
+        private static void TestBigMassive()
+        {
+            //Тестирование поиска в массиве из 100001 элементов 
+            int[] numbers = new int[100001];
+            Random rand = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rand.Next(0, 100);
+            Array.Sort(numbers);
+            numbers[100000] = 100;
+            if (BinarySearch(numbers, 100) != 100000)
+                Console.WriteLine("! Поиск в массиве из 100001 элементов работает не корректно");
+            else
+                Console.WriteLine("Поиск в массиве из 100001 элементов работает корректно");
+        }
+    }
+}
---
+++ LABA2/LABA2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LABA2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("LABA2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("56735b08-4bdb-4884-b3e2-93c63f1ed5b2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
